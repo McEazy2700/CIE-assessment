9c94e5827a2c69dcff0bfb5be109df46d3c743dc
McEazy2700 <codeepoch@gmail.com>
2025-04-16 01:43:12 +0100

    Initial commit

---
+++ users/models/users.py
@@ -0,0 +1,47 @@
+from typing import Any
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager["CustomUser"]):
+
+    def create_user(self, email: str, password: str, **extra_fields: dict[str, Any]):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(
+        self, email: str, password: str, **extra_fields: dict[str, Any]
+    ):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        __ = extra_fields.setdefault("is_staff", True)
+        __ = extra_fields.setdefault("is_superuser", True)
+        __ = extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
